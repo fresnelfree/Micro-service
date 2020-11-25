252a0b85c280217b9dd1698186f54c1e9a57f1e2
teboukeu tsafack fresnel <teboukeufresnel@gmail.com>
2020-11-25 15:02:28 +0100

    Ajout des methodes CRUD

--- src/main/java/fresnel/servicecommande/DAO/CommandeDao.java
+++ src/main/java/fresnel/servicecommande/DAO/CommandeDao.java
@@ -1,10 +1,13 @@
 package fresnel.servicecommande.DAO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import fresnel.servicecommande.Models.Commande;
 
 @Repository
+@RepositoryRestResource
 public interface CommandeDao extends JpaRepository <Commande, Integer> {
   
 }
